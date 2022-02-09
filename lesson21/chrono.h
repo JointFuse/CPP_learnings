daf0050accb3bb172e7e20d7c2d31e1a571def76
JointFuse <57036307+JointFuse@users.noreply.github.com>
2022-02-09 17:51:47 +0300

    Add files via upload

---
+++ lesson21/chrono.h
@@ -0,0 +1,55 @@
+#ifndef Chrono_h
+#define Chrono_h
+
+#include <stdexcept>
+#include <iostream>
+
+namespace Chrono {
+	using namespace std;
+
+	enum class Month {
+		jan = 1, feb, mar, apr, may, jun,
+		jul, aug, sep, oct, nov, dec
+	};
+
+	enum class Day {
+		mon = 1, tue, wed,
+		thu, fri, sat, sun
+	};
+
+	struct date {
+		date(int y, Month m, int d);
+		date() : y{ 1970 }, m{ Month::jan }, d{ 1 }, sum{ 1 } {}
+
+		int y, d, sum;
+		Month m;
+	};
+
+	class Date {
+	private:
+		long int d;
+	public:
+		Date(date d);
+
+		void add_day(int n) { d += n; }
+		void add_month();
+		void add_year();
+
+		date convert()	const;
+		int day()		const { date d{ convert() }; return d.d; }
+		Month month()	const { date d{ convert() }; return d.m; }
+		int year()		const { date d{ convert() }; return d.y; }
+	};
+
+	bool leapyear(int y);
+	int month_to_days(Month m, int y);
+	int days_in_date(date d);
+	Day day_of_week(Date);
+	Date next_sunday(Date);
+	Day next_weekday(Date);
+	
+	ostream& operator<< (ostream&, const Chrono::Date&);
+	ostream& operator<< (ostream& os, const Day& d);
+	istream& operator>> (istream&, Chrono::date&);
+}
+#endif	// !Chrono_h
